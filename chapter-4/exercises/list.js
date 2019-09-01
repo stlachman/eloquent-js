b1d553f981b8cc8eb6dd6a539629b5321aa4cd5e
Seth Lachman <lachmanseth17@gmail.com>
2019-09-01 14:40:49 -0400

    Added nth function to list exercises.

--- chapter-4/exercises/list.js
+++ chapter-4/exercises/list.js
@@ -8,7 +8,8 @@ const arrayToList = arr => {
   return list;
 };
 
-arrayToList([1, 2, 3]);
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
 
 const listToArray = list => {
   const arr = [];
@@ -23,6 +24,8 @@ const listToArray = list => {
   }
   return arr;
 };
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
 
 const prepend = (element, list) => {
   let newList = {};
@@ -31,3 +34,23 @@ const prepend = (element, list) => {
 
   return newList;
 };
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+
+const nth = (list, number) => {
+  let value;
+  let node = list;
+  let counter = 0;
+
+  while (node) {
+    if (counter == number) {
+      return node.value;
+    }
+    counter++;
+    node = node.rest;
+  }
+  return value;
+};
+
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
