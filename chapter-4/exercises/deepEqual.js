d94a8795e0b128af7b9d6c8b9e3c566188ef2d68
Seth Lachman <lachmanseth17@gmail.com>
2019-10-13 10:45:38 -0400

    Updated deepEqual solution.

--- chapter-4/exercises/deepEqual.js
+++ chapter-4/exercises/deepEqual.js
@@ -1,23 +1,28 @@
 let obj = { here: { is: "an" }, object: 2 };
 
-function isObj(val) {
-  return typeof val === "object" && typeof val !== null;
-}
+const checkIfObj = val => typeof val === "object" && val !== null;
 
-function deepEqual(val1, val2) {
+const deepEqual = (val1, val2) => {
   const keys1 = Object.keys(val1);
   const keys2 = Object.keys(val2);
+
   if (keys1.length !== keys2.length) return false;
 
   for (let i = 0; i < keys1.length; i++) {
-    if (isObj(val1[keys1[i]]) && isObj(val2[keys2[i]])) {
-      deepEqual(val1[keys1[i]], val2[keys2[i]]);
-    } else if (val1[keys1[i]] !== val2[keys2[i]]) {
+    let first = val1[keys1[i]];
+    let second = val2[keys2[i]];
+
+    if (checkIfObj(first) && checkIfObj(second)) {
+      if (!deepEqual(first, second)) {
+        return false;
+      }
+    } else if (first !== second) {
       return false;
     }
   }
   return true;
-}
+};
+
 // deepEqual(obj, obj);
 // // true
 // deepEqual(obj, { here: 1, object: 2 });
